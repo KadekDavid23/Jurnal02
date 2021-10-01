ea7d6ace2083badf6df025b86be6160287d0674e
KadekDavid23 <davidkurniawan332@gmail.com>
2021-10-01 08:55:49 +0700

    menambahkan file Jurnal.java

---
+++ Jurnal.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author orang
+ */
+public class Jurnal {
+ 
+
+	public static void main(String[] args) {
+		
+		String nama,nim,kelas=null;
+		
+		
+		System.out.println("Nama 	: Kadek David Kurniawan");
+                System.out.println("NIM 	: 6706210126");
+                System.out.println("Kelas 	: D3IF-45-02");
+              
+                
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
+		
+	
+		
+		
+		
+		
+
+
+        
+   
