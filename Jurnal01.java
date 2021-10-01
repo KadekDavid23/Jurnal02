2e7005f4dc8b02746f61bb463c5eaa24bc298885
KadekDavid23 <91107146+KadekDavid23@users.noreply.github.com>
2021-10-01 11:23:12 +0700

    menambahkan file Jurnal.java

---
+++ Jurnal01.java
@@ -0,0 +1,25 @@
+public class Jurnal {
+ 
+
+	public static void main(String[] args) {
+		
+		String nama,nim,kelas=null;
+		
+		
+		System.out.println("Nama 	: Kadek David Kurniawan");
+                System.out.println("NIM 	: 6706210126");
+                System.out.println("Kelas 	: D3IF-45-02");
+              
+                
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Jurnal02
+Tugas Praktikum IMA
